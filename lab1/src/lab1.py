41a608c50807f54236bfd4472c9346cd38a378eb
Giuliano R. Pinheiro <giulianorp2010@gmail.com>
2018-03-19 15:05:47 -0300

    GD started working
    I changed a couple things maybe and added most of the model validation
    code (MSE and a comparison with sklearn's LinearRegression).

--- lab1/src/lab1.py
+++ lab1/src/lab1.py
@@ -3,7 +3,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import tensorflow as tf
 
-train_X = pd.read_csv('train.csv')
+train_X = pd.read_csv('../data/train.csv')
 train_y = train_X['shares']
 
 # Feature selection
@@ -14,18 +14,33 @@ train_X.drop('url', axis=1, inplace=True)
 train_X = train_X.astype(np.float32)
 train_y = train_y.astype(np.float32)
 
-m, n = train_X.shape
+# some features are scaled to not-so-reasonable ranges
+train_X['timedelta'] = train_X['timedelta'].apply(lambda x: x / 10)
+train_X['n_tokens_content'] = train_X['n_tokens_content'].apply(lambda x: x / 10)
+train_X['kw_max_min'] = train_X['kw_max_min'].apply(lambda x: x / 100)
+train_X['kw_avg_min'] = train_X['kw_avg_min'].apply(lambda x: x / 10)
+train_X['kw_min_max'] = train_X['kw_min_max'].apply(lambda x: x / 1000)
+train_X['kw_max_max'] = train_X['kw_max_max'].apply(lambda x: x / 10000)
+train_X['kw_avg_max'] = train_X['kw_avg_max'].apply(lambda x: x / 10000)
+train_X['kw_min_avg'] = train_X['kw_min_avg'].apply(lambda x: x / 100)
+train_X['kw_max_avg'] = train_X['kw_max_avg'].apply(lambda x: x / 100)
+train_X['kw_avg_avg'] = train_X['kw_avg_avg'].apply(lambda x: x / 100)
+train_X['self_reference_min_shares'] = train_X['self_reference_min_shares'].apply(lambda x: x / 100)
+train_X['self_reference_max_shares'] = train_X['self_reference_max_shares'].apply(lambda x: x / 1000)
+train_X['self_reference_avg_sharess'] = train_X['self_reference_avg_sharess'].apply(lambda x: x / 100)
 
+m, n = train_X.shape
 
 # tf workd with batches
 batch_size = 50
 # we also need learning rate and iterations and step (for logging only)
 learning_rate = 0.00001
-iterations = 1000
-step = 50
+iterations = 1
+step = 20
 
 # init placeholders
-x = tf.placeholder(shape=[None, n], dtype=tf.float32, name='x_i')
+x = tf.placeholder(dtype=tf.float32, name='x_i')
+#x = tf.placeholder(shape=[None, n], dtype=tf.float32, name='x_i')
 y = tf.placeholder(dtype=tf.float32, name='y_i')
 
 # init regression variables
@@ -60,13 +75,13 @@ with tf.Session() as sess:
             sess.run(gd, feed_dict={x: x_i, y: y_i})
         
         # calculates and stores loss for this it
-        loss = sess.run(loss, feed_dict={x: train_X.as_matrix(), y: train_y.as_matrix()})
+        loss_it = sess.run(loss, feed_dict={x: train_X.as_matrix(), y: train_y.as_matrix()})
         
-        training_loss.append(loss)
+        training_loss.append(loss_it)
         
         # display training loss logs each _step_ iterations
         if (it % step) == 0:
-            print("Iteration %04d, loss %.3f" % (it, loss))
+            print("Iteration %04d, loss %.3f" % (it, loss_it))
 
     print("done")
     
