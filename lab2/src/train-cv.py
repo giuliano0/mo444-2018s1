61d92a94fa5d34a6dc070f798038327414363235
Giuliano Roberto Pinheiro <giulianorp2010@gmail.com>
2018-04-21 16:59:13 -0300

    Some new code (no big change) and submission code
    Submission code is a block of a couple lines or so with
    some commands I used to save a submission CSV file to
    the disk. Thatś not the full stuff, though.

--- lab2/src/train-cv.py
+++ lab2/src/train-cv.py
@@ -13,19 +13,21 @@ from skimage.io import imread
 
 from sklearn.linear_model import LogisticRegression as LogReg
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 from sklearn.svm import SVC, LinearSVC
 
 # Load features
-X = np.load('../data/9patches_rgby_4feats.npy')
-y = np.load('../data/9patches_rgby_4feats_labels.npy')
+X = np.load('../data/noise-features2-dwt-norm/512x512patch_rgby_40corrs.npy')
+y = np.load('../data/noise-features2-dwt-norm/512x512patch_rgby_40corrs_labels.npy')
 
 # Transform X to a matrix because I forgot to do it
 X = np.matrix(X)
 
 # Declare classifiers
 lrc = LogReg(penalty='l2', max_iter=1000, n_jobs=4, multi_class='ovr', solver='liblinear')
-svcc = SVC(kernel='rbf', max_iter=1000, decision_function_shape='ovr')
+svcc = SVC(kernel='rbf', max_iter=-1, decision_function_shape='ovr')
 lsvcc = LinearSVC(penalty='l2', multi_class='ovr', max_iter=1000)
 
 clfs = [lrc, svcc, lsvcc]
@@ -46,3 +48,8 @@ for i, s in enumerate(scores):
     print('Classifier %d scored %.2f +- %.2f' % (i, np.mean(s), np.std(s)))
 
 # Mischief managed
+
+# Test gotta be loaded and saved for submission
+#import pandas as pd
+#df = pd.DataFrame(list(zip(testY, preds)), columns=('fname', 'camera'))
+#df.to_csv('../submissions/noise-features2-gauss.1.csv', index=False)
